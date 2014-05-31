93e4aa9f5dc6e15c6bf320eb5c28f9b32e56396e
skoranga <skoranga@paypal.com>
2014-05-30 17:59:43 -0700

    curl n save

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 node-curlnsave
 ==============
 
-Node module to curl a remote url and save a local file
+Node module to curl a remote url and cache locally
+
+
+### Usage
+
+```javascript
+    var curlnsave = require('curlnsave');
+
+    var curl = curlnsave.create({ outdir: './localCache' });
+    var uri = 'http://www.example.com';
+
+    // first time this call will download and cache the url &
+    // subsequent calls will use the cached copy.
+    curl.fetch(uri, function (err, data) {
+        ...
+        ...
+    });
+```
---
+++ test/curlnsave-test.js
@@ -0,0 +1,92 @@
+/*global describe: false, it: false, before: false, after: false*/
+'use strict';
+
+var fs = require('fs-extra'),
+    url = require('url'),
+    path = require('path'),
+    curlnsave = require('../lib/curlnsave'),
+    assert = require('chai').assert;
+
+var URLS = {
+    sample1: 'https://github.com/skoranga',
+    sample2: 'http://www.ebay.com'
+};
+
+
+describe('curlnsave', function () {
+
+    describe('fetch', function () {
+
+        var curl, testOutDir = './test/tempfolder';
+
+        before(function () {
+            fs.deleteSync(testOutDir);
+            curl = curlnsave.create({ outdir: testOutDir });
+        });
+
+
+        after(function () {
+            fs.deleteSync(testOutDir);
+        });
+
+
+        it('should load and cache remote files', function (next) {
+            this.timeout(5000);
+            var uri, filename, location;
+
+            uri = URLS.sample1;
+            filename = url.parse(uri).pathname;
+            filename = path.basename(filename);
+            location = path.join(testOutDir, filename);
+
+            curl.fetch(uri, function (err, data, fromCache) {
+                assert.isNotObject(err);
+                assert.isDefined(data);
+                assert.isFalse(fromCache);
+                assert.isTrue(fs.existsSync(location));
+
+                next();
+            });
+        });
+
+
+        it('should support an optional local name', function (next) {
+            this.timeout(5000);
+            var uri, filename, location;
+
+            uri = URLS.sample2;
+            filename = 'smurf.txt';
+            location = path.join(testOutDir, filename);
+
+            curl.fetch(uri, filename, function (err, data, fromCache) {
+                assert.isNotObject(err);
+                assert.isDefined(data);
+                assert.isFalse(fromCache);
+                assert.isTrue(fs.existsSync(location));
+
+                next();
+            });
+        });
+
+
+        it('should serve previously requested files from disk', function (next) {
+            this.timeout(5000);
+            var uri, filename, location;
+
+            uri = URLS.sample2;
+            filename = 'smurf.txt';
+            location = path.join(testOutDir, filename);
+
+            curl.fetch(uri, filename, function (err, data, fromCache) {
+                assert.isNotObject(err);
+                assert.isDefined(data);
+                assert.isTrue(fromCache);
+                assert.isTrue(fs.existsSync(location));
+
+                next();
+            });
+        });
+
+    });
+
+});
